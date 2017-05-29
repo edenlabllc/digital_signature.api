3a9106b57f962dc4b40a5e175e23cb7412142f45
MBatrak <maxim.batrak@finstar.com>
2017-05-29 22:08:41 +0300

    Tasks fix

--- bin/hooks/pre-start.sh
+++ bin/hooks/pre-start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # `pwd` should be /opt/digital_signature_api
 APP_NAME="digital_signature_api"
+
+if [ "${DB_MIGRATE}" == "true" ]; then
+  echo "[WARNING] Migrating database!"
+  ./bin/$APP_NAME command "Elixir.DigitalSignature.ReleaseTasks" migrate!
+fi;
