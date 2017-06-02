f929aaee71c2ffccb11af05f634b303d0031e9c2
MBatrak <maxim.batrak@finstar.com>
2017-06-02 15:50:02 +0300

    Added some checks and optimizations

--- lib/digital_signature_api/web/controllers/digital_signatures_controller.ex
+++ lib/digital_signature_api/web/controllers/digital_signatures_controller.ex
@@ -19,27 +19,24 @@ defmodule DigitalSignature.Web.DigitalSignaturesController do
   end
 
   defp process_signed_data(:error, _conn, _params) do
-    {:error, [{%{description: "not a base64 string", params: ["signed_content"], rule: :invalid}, "$.signed_content"}]}
+    {:error, [{%{description: "not a base64 string", params: [], rule: :invalid}, "$.signed_content"}]}
   end
   defp process_signed_data({:ok, signed_content}, conn, params) do
-    digital_signature_info =
-      signed_content
-      |> :erlang.binary_to_list()
-      |> :digital_signature_lib.processPKCS7Data(CertAPI.get_certs_map())
-      |> process_result()
-      |> Map.merge(params)
-    render(conn, "digital_signature.json", digital_signature_info: digital_signature_info)
+    signed_content
+    |> :erlang.binary_to_list()
+    |> :digital_signature_lib.processPKCS7Data(CertAPI.get_certs_map())
+    |> process_result()
+    |> render_response(params, conn)
   end
 
-  defp process_result({:error, error}), do: Logger.error(error)
+  defp process_result({:error, error}), do: {:error, error}
   defp process_result({:ok, result}) do
     result
-    |> Map.update!(:content, &process_content/1)
-    |> Map.update!(:is_valid, &process_is_valid/1)
+    |> Map.get(:content)
+    |> process_content()
+    |> update_content(result)
+    |> process_is_valid()
   end
-
-  defp process_is_valid(1), do: true
-  defp process_is_valid(0), do: false
 
   defp process_content(content) do
     content
@@ -47,6 +44,24 @@ defmodule DigitalSignature.Web.DigitalSignaturesController do
     |> return_content(content)
   end
 
-  defp return_content({:ok, decoded_content}, _content), do: decoded_content
-  defp return_content(_error, content), do: content
+  defp update_content({:ok, content}, result), do: {:ok, Map.put(result, :content, content)}
+  defp update_content({:error, error}, _result), do: {:error, error}
+
+  defp process_is_valid({:ok, %{is_valid: 1} = result}), do: {:ok, Map.put(result, :is_valid, true)}
+  defp process_is_valid({:ok, %{is_valid: 0} = result}), do: {:ok, Map.put(result, :is_valid, false)}
+  defp process_is_valid({:error, error}), do: {:error, error}
+
+  defp return_content({:ok, decoded_content}, _content), do: {:ok, decoded_content}
+  defp return_content(_error, content) do
+    case String.valid?(content) do
+      true -> {:ok, content}
+      _ -> {:error, [{%{description: "not a valid utf8 string", params: [], rule: :invalid}, "$.content"}]}
+    end
+  end
+
+  defp render_response({:error, error}, _params, _conn) when is_list(error), do: {:error, error}
+  defp render_response({:error, error}, _params, _conn), do: Logger.error(error)
+  defp render_response({:ok, result}, params, conn) do
+    render(conn, "digital_signature.json", digital_signature_info: Map.merge(result, params))
+  end
 end
