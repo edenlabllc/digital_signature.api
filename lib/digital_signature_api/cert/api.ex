697c17cd4557992c05415f59de40108ba2626c35
ybod <yuriibodarev@gmail.com>
2017-12-06 16:35:16 +0200

    Updates to cert API and Yarn script

--- lib/digital_signature_api/cert/api.ex
+++ lib/digital_signature_api/cert/api.ex
@@ -7,40 +7,23 @@ defmodule DigitalSignature.Cert.API do
 
   def get_certs_map do
     query = from p in Cert,
-      where: (p.type == "root" or p.type == "tsp") and p.active,
+      where: p.type in ["root", "tsp"] and p.active,
       left_join: c in Cert,
       on: c.parent == p.id and c.type == "ocsp" and c.active,
       select: {p.type, p.data, c.data}
 
-
     query
     |> Repo.all()
-    |> generate_map()
+    |> Enum.reduce(%{general: [], tsp: []}, &process_cert(&1, &2))
   end
 
-  defp generate_map(certs) do
-    map = %{
-      general: [],
-      tsp: []
-    }
-    Enum.reduce(certs, map, &process_cert(&1, &2))
+  defp process_cert({"root", rootCert, ocspCert}, %{general: general} = map) do
+    new_root = %{root: prepare_cert_data(rootCert), ocsp: prepare_cert_data(ocspCert)}
+
+    Map.put(map, :general, [new_root | general])
   end
-
-  defp process_cert({"root", rootCert, ocspCert}, map) do
-    general =
-      map
-      |> Map.get(:general)
-      |> Kernel.++([%{root: prepare_cert_data(rootCert), ocsp: prepare_cert_data(ocspCert)}])
-
-    Map.put(map, :general, general)
-  end
-  defp process_cert({"tsp", tspCert, _}, map) do
-    tsp =
-      map
-      |> Map.get(:tsp)
-      |> Kernel.++([prepare_cert_data(tspCert)])
-
-    Map.put(map, :tsp, tsp)
+  defp process_cert({"tsp", tspCert, _}, %{tsp: tsp} = map) do
+    Map.put(map, :tsp, [prepare_cert_data(tspCert) | tsp])
   end
 
   defp prepare_cert_data(nil), do: nil
