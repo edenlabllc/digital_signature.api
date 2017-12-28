57b87b7f9ab12600f10130373e7c05a9bab6651c
ybod <yuriibodarev@gmail.com>
2017-12-28 12:40:54 +0200

    [patch] fix module doc

--- lib/digital_signature_api/web/views/invalid_content_view.ex
+++ lib/digital_signature_api/web/views/invalid_content_view.ex
@@ -1,4 +1,7 @@
 defmodule DigitalSignature.Web.InvalidContentView do
+  @moduledoc """
+  This view will render 422 Error in a case of invalid content (malformed Json) inside a correct signed request
+  """
 
   use DigitalSignature.Web, :view
 
