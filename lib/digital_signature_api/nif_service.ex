5864d3cedf89159b9921dc5f0ef9c8f67747dbda
Yurii Bodarev <yuriibodarev@gmail.com>
2018-04-19 19:25:32 +0300

    Replace Poolboy workers pool with single GenServer to work with NIF lib sequentially (#28)

--- lib/digital_signature_api/nif_service.ex
+++ lib/digital_signature_api/nif_service.ex
@@ -1,10 +1,28 @@
 defmodule DigitalSignature.NifService do
   @moduledoc false
-  alias DigitalSignature.Poolboy
+  use GenServer
+  alias DigitalSignature.CertCache
+
+  # Callbacks
+  def init(_) do
+    {:ok, nil}
+  end
+
+  def handle_call({:process_signed_data, signed_data, check}, _from, nil) do
+    check = unless is_boolean(check), do: true
+    {:ok, certs} = CertCache.get_certs()
+
+    result = DigitalSignatureLib.processPKCS7Data(signed_data, certs, check)
+
+    {:reply, result, nil, :hibernate}
+  end
+
+  # Client
+  def start_link do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
 
   def process_signed_data(signed_data, check) do
-    :poolboy.transaction(Poolboy.pool_name(), fn worker ->
-      GenServer.call(worker, {:process_signed_data, signed_data, check})
-    end)
+    GenServer.call(__MODULE__, {:process_signed_data, signed_data, check})
   end
 end
