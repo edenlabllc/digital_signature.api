fb85227bd5987deef42590b5a1687b6777c0f108
ybod <yuriibodarev@gmail.com>
2018-05-02 15:25:33 +0300

    Move logic from controller into a separate module

--- lib/digital_signature_api/nif_service.ex
+++ lib/digital_signature_api/nif_service.ex
@@ -8,11 +8,11 @@ defmodule DigitalSignature.NifService do
     {:ok, nil}
   end
 
-  def handle_call({:process_signed_data, signed_data, check}, _from, nil) do
+  def handle_call({:process_signed_content, signed_content, check}, _from, nil) do
     check = unless is_boolean(check), do: true
     {:ok, certs} = CertCache.get_certs()
 
-    result = DigitalSignatureLib.processPKCS7Data(signed_data, certs, check)
+    result = DigitalSignatureLib.processPKCS7Data(signed_content, certs, check)
 
     {:reply, result, nil, :hibernate}
   end
@@ -22,7 +22,7 @@ defmodule DigitalSignature.NifService do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  def process_signed_data(signed_data, check) do
-    GenServer.call(__MODULE__, {:process_signed_data, signed_data, check})
+  def process_signed_content(signed_content, check) do
+    GenServer.call(__MODULE__, {:process_signed_content, signed_content, check})
   end
 end
